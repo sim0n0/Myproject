6b5bc6aac1054de9661acaf2de2e5cd5a4fc3667
delisle <delislesimon6@gmail.com>
2020-04-05 22:31:05 +0200

    Ajout appel serveur Pokémon

---
+++ app/src/main/java/com/example/myproject/PokeApi.java
@@ -0,0 +1,11 @@
+package com.example.myproject;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PokeApi {
+
+    @GET("/api/v2/pokemon")
+    Call<RestPokemonResponse> getPokemonResponse();
+
+}
